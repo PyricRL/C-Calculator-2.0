15fd791d0ca25b5023a5b4a2b284778ce85c05cb
PyricRL <ljcosa@icloud.com>
2024-02-08 09:49:22 -0600

    plans

--- Main.c
+++ Main.c
@@ -39,8 +39,9 @@ int main()
         break;
     }
 
-    // coment lol
-    //another comment
+    // Next we need to work on rounding based on the output.
+    // If it is a 6.354300 we need to chop off the 00 at the end
+    // If it is a full number than we remove all 0s after decimal
 
     printf("\nresult: %lf", result);
 
